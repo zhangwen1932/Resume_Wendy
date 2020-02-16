6a85fa0bb9ac9128cefb7b92f2217746e16046d5
Zhang Wen <zhangwen1932@hotmail.com>
2020-02-17 00:59:04 +0800

    添加json文件

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var callback = function () {
+const callback = () => {
   $('.skill-item').each(function () {
     newWidth = $(this).parent().width() * $(this).data('percent');
     $(this).width(0);
@@ -6,26 +6,51 @@ var callback = function () {
       width: newWidth,
     }, 1000);
   });
-  $('.icons-red').each(function () {
+  $('.icons-red').each(() => {
     height = $(this).height();
     $(this).animate({
       height: 14,
     }, 2000);
   });
 };
-$(document).ready(function() {
+$(document).ready(() => {
   $('#status').fadeOut();
   $('#preloader').delay(350).fadeOut('slow');
   $('body').delay(350).css({
     'overflow': 'visible'
   });
   callback();
+  $.ajax({
+    method: "GET",
+    url: "./src/documents/info.json",
+    cache: false,
+    dataType: "text"
+  }).done((data)=>{
+    const info = $.parseJSON(data);
+    if(info.success){
+      const userInfo = info.data;
+      // 渲染about板块
+      const about = userInfo.about;
+      // 渲染教育板块
+      const eduction = userInfo.eduction;
+      // 渲染经历板块
+      const experience = userInfo.experience;
+      // 渲染联系板块
+      const contact = userInfo.contact;
+      // 渲染技能板块
+      const skill = userInfo.skill;
+      // 渲染爱好板块
+      const hobbies = userInfo.hobbies;
+    }
+  }).fail((error)=>{
+    console.log('error', error);
+  });
 });
 
-var resize;
-window.onresize = function () {
+let resize;
+window.onresize = () => {
   clearTimeout(resize);
-  resize = setTimeout(function () {
+  resize = setTimeout(() => {
     callback();
   }, 100);
 };
