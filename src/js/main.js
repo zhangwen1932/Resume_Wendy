50dd35cfd86e158808635c529b1be807e421d5f6
Zhang Wen <zhangwen1932@hotmail.com>
2020-02-17 01:49:13 +0800

    渲染页面

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 const callback = () => {
-  $('.skill-item').each(function () {
-    newWidth = $(this).parent().width() * $(this).data('percent');
-    $(this).width(0);
-    $(this).animate({
-      width: newWidth,
-    }, 1000);
-  });
+  // $('.skill-item').each(function () {
+  //   newWidth = $(this).parent().width() * $(this).data('percent');
+  //   $(this).width(0);
+  //   $(this).animate({
+  //     width: newWidth,
+  //   }, 1000);
+  // });
   $('.icons-red').each(() => {
     height = $(this).height();
     $(this).animate({
@@ -29,18 +29,107 @@ $(document).ready(() => {
     const info = $.parseJSON(data);
     if(info.success){
       const userInfo = info.data;
+      console.log('userInfo', userInfo);
       // 渲染about板块
       const about = userInfo.about;
+      $('#about').html(about);
       // 渲染教育板块
       const eduction = userInfo.eduction;
+      let edcutionRender = '';
+      if(eduction.length>0){
+        eduction.forEach(item => {
+          edcutionRender += 
+          `<li>
+            <div class="row">
+              <div class="year pull-left">${item.time}</div>
+              <div class="description pull-right">
+                <h3>${item.school}</h3>
+                <p>${item.content}/p>
+              </div>
+            </div>
+          </li>`
+        });
+        $('#education').html(edcutionRender);
+      }
       // 渲染经历板块
       const experience = userInfo.experience;
+      let experienceRender = '';
+      if (experience.length>0){
+        experience.forEach((item) => {
+          experienceRender +=
+            `
+        <div class="job clearfix">
+            <div class="row">
+              <div class="col-3">
+                <div class="where">${item.company}</div>
+                <div class="year">${item.time}</div>
+              </div>
+              <div class="col-9">
+                <div class="profession">${item.position}</div>
+                <div class="description">
+                  ${item.content}
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+        })
+        $('#job').html(experienceRender);
+      }
       // 渲染联系板块
       const contact = userInfo.contact;
+      let contactRender = '';
+      if(contact.length > 0){
+        contact.forEach((item) => {
+          contactRender += `
+          <div class="contact-item">
+            <div class="row">
+              <div class="col-2">
+                <div class="icon pull-left text-center">
+                  <span class="iconfont icon-icon-test3"></span>
+                </div>
+              </div>
+              <div class="col-10">
+                <div class="title pull-right">${item.nickname}</div>
+                <div class="description pull-right">${item.content}</div>
+              </div>
+            </div>
+          </div>
+          `
+        })
+        $("#contact").html(contactRender);
+      }
       // 渲染技能板块
       const skill = userInfo.skill;
+      let skillRender = '';
+      if(skill.length>0){
+        skill.forEach((item) => {
+          skillRender += `
+          <div class="skill-item" data-percent="${item.percent}">
+              ${item.name}
+            </div>
+          `
+        })
+        $('#skill').html(skillRender);
+        $('.skill-item').each(function () {
+          newWidth = $(this).parent().width() * $(this).data('percent');
+          $(this).width(0);
+          $(this).animate({
+            width: newWidth,
+          }, 1000);
+        });
+      }
       // 渲染爱好板块
       const hobbies = userInfo.hobbies;
+      let hobbiesRender = '';
+      if(hobbies.length>0){
+        hobbies.forEach((item) => {
+          hobbiesRender += `
+          <div class="hobby-item">${item.name}</div>
+          `
+        })
+        $('#hobbies').html(hobbiesRender);
+      }
     }
   }).fail((error)=>{
     console.log('error', error);
