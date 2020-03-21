1476e0fb78eaf4f41054aec7d4ad22484aebde43
Zhang Wen <zhangwen1932@hotmail.com>
2020-03-21 20:40:19 +0800

    update

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -10,6 +12,7 @@ module.exports = {
       chunks: "index",
       inject: "body" 
     }),
+    new CleanWebpackPlugin(),
   ],
   output: {
     filename: 'bundle.js',
